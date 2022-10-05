e8603a190c99f3e5f30fb80aa97521b515e6eceb
Hyun Ahn <codepawn@icloud.com>
2022-10-05 19:55:52 +0900

    Update loyalty_service.rb

--- app/services/loyalty_service.rb
+++ app/services/loyalty_service.rb
@@ -8,15 +8,23 @@ class LoyaltyService
 
     point_history = PointHistory.new({
                                        user_id: @user.id,
-                                       transaction_id: transaction_object.id,
+                                       purchase_transaction_id: transaction_object.id,
                                        point_earn: point,
                                      })
 
     point_history.save
+
+    update_user_point_total(point: point)
+
     point_history
   end
 
   private
+
+  def update_user_point_total(point:)
+    new_total_point = @user.profile.point_total + point
+    @user.profile.update({ point_total: new_total_point })
+  end
 
   def point_formula(spend)
     (spend.to_f / 10).round
