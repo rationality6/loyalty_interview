59e02a9fcf0f6e66cd49b2e2a3c776f8715f8022
Hyun Ahn <codepawn@icloud.com>
2022-10-05 19:47:00 +0900

    Update loyalty_service.rb

--- app/services/loyalty_service.rb
+++ app/services/loyalty_service.rb
@@ -1,17 +1,25 @@
 class LoyaltyService
   def initialize(user_object:)
-    super
     @user = user_object
   end
 
-  def perform() end
-
   def save_point(transaction_object:)
-    point = (transaction_object.spend.to_f / 10).round
+    point = point_formula(transaction_object.spend)
 
+    point_history = PointHistory.new({
+                                       user_id: @user.id,
+                                       transaction_id: transaction_object.id,
+                                       point_earn: point,
+                                     })
+
+    point_history.save
+    point_history
   end
 
   private
 
+  def point_formula(spend)
+    (spend.to_f / 10).round
+  end
 end
 
