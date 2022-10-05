487edd209a09adeb54eaa8bb375c885455995473
Hyun Ahn <codepawn@icloud.com>
2022-10-05 20:19:30 +0900

    update and fix specs

--- spec/requests/purchase_transactions_request_spec.rb
+++ spec/requests/purchase_transactions_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Transactions", type: :request do
+RSpec.describe "PurchaseTransactions", type: :request do
   let!(:test_user) {
     user = User.new
     user.save(validate: false)
@@ -15,20 +15,20 @@ RSpec.describe "Transactions", type: :request do
 
   it "when no params spend" do
     expect {
-      post '/api/v1/transactions/purchase', params: {}
+      post '/api/v1/purchase_transactions/purchase', params: {}
     }.to raise_error("no spend params")
   end
 
   it "status check" do
-    post '/api/v1/transactions/purchase', params: { spend: 60 }
+    post '/api/v1/purchase_transactions/purchase', params: { spend: 60 }
     expect(response.status).to(eq(200))
   end
 
-  it "10% point" do
-    expect
-  end
-
-  it "when from foreign country, point * 2 " do
-
-  end
+  # it "10% point" do
+  #   expect
+  # end
+  #
+  # it "when from foreign country, point * 2 " do
+  #
+  # end
 end
