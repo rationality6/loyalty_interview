be2e056205da6b43c6de20d2cf6168f9a8125837
Hyun Ahn <codepawn@icloud.com>
2022-10-06 21:30:13 +0900

    logic with test

---
+++ app/models/reward.rb
@@ -0,0 +1,3 @@
+class Reward < ApplicationRecord
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :purchase_transactions
   has_many :point_histories
+  has_many :rewards
+
+  def self.give_birthday_reward()
+    beginning_of_month = Date.today.beginning_of_month
+    beginning_of_next_month = beginning_of_month.next_month
+    birthday_users = Profile.where(birthday: beginning_of_month..beginning_of_next_month)
+
+    birthday_users.each do |birthday_user|
+      Reward.create(
+        user_id: birthday_user.user_id,
+        reward_name: "Birth day Free Coffee reward",
+      )
+    end
+    birthday_users
+  end
+
 end
