042dbe408ea4a6fbf707a4995d0c218f2dfee33e
Hyun Ahn <codepawn@icloud.com>
2022-10-10 01:41:45 +0900

    Update loyalty_level.rb

--- app/services/loyalty_level.rb
+++ app/services/loyalty_level.rb
@@ -3,11 +3,18 @@ module LoyaltyLevel
   # A gold tier customer is an end user who accumulates 1000 points
   # A platinum tier customer is an end user who accumulates 5000 points
 
-  def level_check
-
+  def level_check_and_lovel_up(user:)
+    if 1000 <= user.profile.total_point
+      level_up(user: user, tier: "gold")
+    elsif 5000 <= user.profile.total_point
+      level_up(user: user, tier: "platinum")
+    end
   end
 
-  def level_up
+  private
 
+  def level_up(user:, tier:)
+    user.update(tier: tier)
   end
+
 end
