cd32c409010510983555504757ad424829947fa7
Hyun Ahn <codepawn@icloud.com>
2022-10-11 16:26:08 +0900

    Update loyalty_level_spec.rb (#11)
    Update loyalty_level_spec.rb

--- spec/requests/purchase_transactions/loyalty_level_spec.rb
+++ spec/requests/purchase_transactions/loyalty_level_spec.rb
@@ -18,6 +18,16 @@ RSpec.describe "LoyaltyLevel", type: :request do
       post purchase_purchase_transactions_url, params: { spend: 50000, test_user_id: test_user.id }
       expect(User.find(test_user.id).profile.tier).to eq('platinum')
     end
+
+    it "when user got promoted to gold got airport reward" do
+      expect(test_user.profile.tier).to eq('standard')
+      post purchase_purchase_transactions_url, params: { spend: 10000, test_user_id: test_user.id }
+      the_user = User.find(test_user.id)
+      expect(the_user.profile.tier).to eq('gold')
+      reward_present = Reward.where({ user_id: the_user, reward_name: "4x Airport Lounge Access Reward" }).present?
+      expect(reward_present).to eq(true)
+    end
+
   end
 
 end
